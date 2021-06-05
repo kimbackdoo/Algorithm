45bf832c762adf4c589404c7ac12845745536ae4
kimbackdoo <45621561+kimbackdoo@users.noreply.github.com>
2021-06-05 11:01:43 +0900

    Update 왕실의 나이트.py

--- Python/왕실의 나이트.py
+++ Python/왕실의 나이트.py
@@ -1,18 +1,14 @@
 # ord : 문자를 아스키코드 값으로 바꾸는 내장함수
 
-start = input()
+def solution(position):
+  x = ord(position[0]) - ord('a') + 1
+  y = int(position[1])
 
-column = ord(start[0]) - ord('a') + 1
-row = int(start[1])
+  moves = [(1, 2), (1, -2), (-1, 2), (-1, -2), (2, 1), (2, -1), (-2, 1), (-2, -1)]
+  answer = 0
+  for move in moves:
+    if (x + move[0]) >= 1 and (x + move[0]) <= 8:
+      if (y + move[1]) >= 1 and (y + move[1]) <= 8:
+        answer += 1
 
-steps = [(2,1), (2,-1), (-2,1), (-2,-1), (1,2), (1,-2), (-1,2), (-1,-2)]
-
-result = 0
-for step in steps:
-  nextRow = row + step[0]
-  nextColumn = column + step[1]
-
-  if nextRow>=1 and nextRow<=8 and nextColumn>=1 and nextColumn<=8:
-    result += 1
-
-print(result)mud
+  return answer
