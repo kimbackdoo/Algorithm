ae7408f00a90f530cbe66237b36242342a3dd924
kimbackdoo <45621561+kimbackdoo@users.noreply.github.com>
2021-06-16 01:24:51 +0900

    Create 캠핑.py

---
+++ Python/BOJ/Greedy/캠핑.py
@@ -0,0 +1,24 @@
+# v에서 p를 계속해서 빼나가면 되므로 그리디
+# v-p가 0보다 클 경우 v에서 p를 빼고 result에 l 값을 더함
+# 그렇지 않으면 즉, 휴가를 다 썼으면 result에 v, l의 최솟값을 더함
+# 이유는 v가 l보다 클 경우, l만큼 사용할 수 있으므로, v가 l보다 작으면 v만큼 사용하면 됨
+# 예를 들어, l=2, p=7, v=20인 경우 14일 동안 4일 캠핑장을 이용할 수 있고 6일이 남았지만 그중 2일만 캠핑장을 이용할 수 있다.
+
+idx = 0
+while True:
+  l, p, v = map(int, input().split())
+
+  if l==0 and p==0 and v==0:
+    break
+
+  result = 0
+  while True:
+    if v-p > 0: # v-p가 0보다 클 경우 반복
+      v -= p
+      result += l
+    else: # 작을경우 즉, 휴가를 다 쓴 경우
+      result += min(v, l) # v, l 중 최솟값을 result에 더함
+      break
+
+  idx += 1
+  print("Case " + str(idx) + ": " + str(result))
