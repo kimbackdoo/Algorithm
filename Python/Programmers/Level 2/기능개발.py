0052b15372b5247f1f67d686c72c7b345a91e981
김재훈 <kdk7121743@naver.com>
2021-06-23 11:40:40 +0900

    Update 기능개발.py

--- Python/Programmers/Level 2/기능개발.py
+++ Python/Programmers/Level 2/기능개발.py
@@ -18,20 +18,39 @@
 #     answer.append(cnt)
 #     return answer
 
+# def solution(progresses, speeds):
+#     answer, stack = [], []
+#     cnt = 1
+#     for i in range(len(progresses)):
+#         speed = 0 # 수행 속도 계산하기 위함
+#         while progresses[i] < 100: # 각 기능당 수행속도를 계산
+#             progresses[i] += speeds[i]
+#             speed += 1
+        
+#         if stack and stack[-1] >= speed: # stack이 비어있지 않고 stack[-1]의 값이 수행속도보다 크거나 값을 경우
+#             answer[-1] += 1 # 배포될 기능 개수 +1
+#             continue
+#         # 첫번째 기능은 다른 기능과 수행속도를 비교 못하므로 append
+#         stack.append(speed)
+#         answer.append(cnt)
+        
+#     return answer
+
+import math
+
 def solution(progresses, speeds):
-    answer, stack = [], []
-    cnt = 1
+    stack = []
     for i in range(len(progresses)):
-        speed = 0 # 수행 속도 계산하기 위함
-        while progresses[i] < 100: # 각 기능당 수행속도를 계산
-            progresses[i] += speeds[i]
-            speed += 1
-        
-        if stack and stack[-1] >= speed: # stack이 비어있지 않고 stack[-1]의 값이 수행속도보다 크거나 값을 경우
-            answer[-1] += 1 # 배포될 기능 개수 +1
+        stack.append(math.ceil((100 - progresses[i]) / speeds[i])) # 각 기능이 완료되는 날짜, 반복문으로 계산하지 않고 바로 계산
+
+    answer = [1] # 첫번째 기능은 무조건 배포되므로 answer에 초기값으로 1 저장
+    chk = stack[0] # 첫번째 기능은 무조건 배포되므로 비교수로 첫번째 기능이 완료된 날짜를 저장
+    for i in range(1, len(stack)):
+        if chk >= stack[i]: # 만약 chk가 다른 기능의 날짜를 포함하고 있으면
+            answer[-1] += 1 # answer 마지막 값 +1
             continue
-        # 첫번째 기능은 다른 기능과 수행속도를 비교 못하므로 append
-        stack.append(speed)
-        answer.append(cnt)
-        
+
+        chk = stack[i] # 아니라면 chk 값 변경
+        answer.append(1) # 1 추가
+
     return answer
