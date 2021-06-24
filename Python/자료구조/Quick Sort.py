0d9b27948658db17fed1bc57672b5a6aea7120e8
김재훈 <kdk7121743@naver.com>
2021-06-24 10:21:30 +0900

    Create Quick Sort.py

---
+++ Python/자료구조/Quick Sort.py
@@ -0,0 +1,11 @@
+def quick_sort(arr):
+  if len(arr) <= 1:
+    return arr
+  
+  pivot = arr[0]
+  tail = arr[1:]
+
+  left_side = [x for x in tail if x <= pivot]
+  right_side = [x for x in tail if x > pivot]
+
+  return quick_sort(left_side) + [pivot] + quick_sort(right_side)
