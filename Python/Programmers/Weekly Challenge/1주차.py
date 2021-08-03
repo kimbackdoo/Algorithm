4a828a0ff9aae56b00cfd2c4aa9a16ff74c66fe4
김재훈 <kdk7121743@naver.com>
2021-08-03 21:06:20 +0900

    Update 1주차.py

--- Python/Programmers/Weekly Challenge/1주차.py
+++ Python/Programmers/Weekly Challenge/1주차.py
@@ -8,8 +8,8 @@
 #     answer = sum([price * i for i in range(1, count + 1)]) - money
 #     return max(0, answer) # if문을 사용하지 않고 max 함수를 이용하여 return
 
-# 총 필요한 이용금액은 price * 1 + price * 2 + price * 3 + price * 4 + ... + price * (count + 1)
-# 즉, price * (1 + 2 + 3 + 4 + ... + count + 1)이므로 price * count * (count + 1) // 2 공식을 세울 수 있음
+# 총 필요한 이용금액은 price * 1 + price * 2 + price * 3 + price * 4 + ... + price * count
+# 즉, price * (1 + 2 + 3 + 4 + ... + count)이므로 price * count * (count + 1) // 2 공식을 세울 수 있음
 
 def solution(price, money, count):
     answer = price * count * (count + 1) // 2 - money # 공식 적용
