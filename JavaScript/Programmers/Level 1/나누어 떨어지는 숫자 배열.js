2b26d725e7ea310b94ad1ec9c22e93c9fc34753c
김재훈 <kdk7121743@naver.com>
2021-08-15 10:55:58 +0900

    Create 나누어 떨어지는 숫자 배열.js

---
+++ JavaScript/Programmers/Level 1/나누어 떨어지는 숫자 배열.js
@@ -0,0 +1,7 @@
+// arr에서 모든 요소를 divisor로 나누었을 때 나누어 떨어지는 것만 필터링 즉, filter 메소드 이용
+// 필터링 된 배열의 길이가 1 이상이면 sort, 0이면 [-1] 반환
+
+function solution(arr, divisor) {
+    const answer = arr.filter(num => !(num % divisor)); // divisor로 나누어 떨어지는 요소만 필터링
+    return answer.length ? answer.sort((a, b) => a - b) : [-1]; // answer의 길이가 있으면 sort, 0이면 [-1] return
+}
