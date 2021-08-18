20b0b5b300251e75e418b7933d9f780d9ab080b6
김재훈 <kdk7121743@naver.com>
2021-08-18 12:03:31 +0900

    Create 문자열을 정수로 바꾸기.js

---
+++ JavaScript/Programmers/Level 1/문자열을 정수로 바꾸기.js
@@ -0,0 +1,12 @@
+// parseInt(s) 함수를 이용하여 s를 숫자로 바꿔줌, 이때 부호도 같이 바꿔줌
+// 예를 들어, "-1234"면 -1234로 변환
+
+// function solution(s) {
+//     return parseInt(s);
+// }
+
+// + 연산자를 사용하여 문자열을 숫자로 
+
+function solution(s) {
+    return +s;
+}
