26211f45fbf0dff18ff5c6d34da73ee60983ce9e
김재훈 <kdk7121743@naver.com>
2021-08-21 09:19:59 +0900

    Create 정수 제곱근 판별.js

---
+++ JavaScript/Programmers/Level 1/정수 제곱근 판별.js
@@ -0,0 +1,7 @@
+// n이 x의 제곱인지 아닌지 확인
+// parseInt 함수를 사용하여 n의 제곱근이 정수인지 확인
+
+function solution(n) {
+    // n의 제곱근이 정수면 n의 제곱근 + 1의 제곱 return, 아니면 -1 return
+    return parseInt(n ** 0.5) === n ** 0.5 ? (n ** 0.5 + 1) ** 2 : -1;
+}
