f5b67414f3e694372c1f3a3870120a8fc3220e78
김재훈 <kdk7121743@naver.com>
2021-08-23 11:27:53 +0900

    Create x만큼 간격이 있는 n개의 숫자.js

---
+++ JavaScript/Programmers/Level 1/x만큼 간격이 있는 n개의 숫자.js
@@ -0,0 +1,16 @@
+// x부터 시작하여 x씩 증가하는 숫자를 n개 가진 배열을 만들면 됨
+
+// function solution(x, n) {
+//     const answer = [];
+//     for (let i = 1; i <= n; i++) {
+//         answer.push(i * x); // i * x를 하게 되면 x씩 증가하는 숫자가 됨 즉, (현재 인덱스 + 1) * x
+//     }
+    
+//     return answer;
+// }
+
+function solution(x, n) {
+    // new Array를 이용하여 크기가 n인 배열을 만든 후, 모든 요소를 0으로 채움
+    // 이후, map 메소드를 이용하여 각 요소를 (각 요소의 인덱스 + 1) * x로 mapping
+    return new Array(n).fill(0).map((_, i) => (i + 1) * x);
+}
