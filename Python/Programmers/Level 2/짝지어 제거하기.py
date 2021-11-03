9b31a24390373c401d727e0352388667e551deb9
김재훈 <kdk7121743@naver.com>
2021-11-04 00:19:59 +0900

    Update 짝지어 제거하기.py

--- Python/Programmers/Level 2/짝지어 제거하기.py
+++ Python/Programmers/Level 2/짝지어 제거하기.py
@@ -1,10 +1,21 @@
+# def solution(s):
+#     answer = []
+#     for word in s:
+#         if answer and answer[-1] == word: # answer가 비어있지 않거나 answer[-1]이 word와 같다면
+#             answer.pop() # answer에서 마지막 값 pop
+#             continue
+            
+#         answer.append(word) # 그렇지 않으면 word append
+    
+#     return 1 if len(answer) == 0 else 0
+
 def solution(s):
     answer = []
-    for word in s:
-        if answer and answer[-1] == word: # answer가 비어있지 않거나 answer[-1]이 word와 같다면
-            answer.pop() # answer에서 마지막 값 pop
+    for alpha in s:
+        if answer and answer[-1] == alpha:
+            answer.pop()
             continue
-            
-        answer.append(word) # 그렇지 않으면 word append
+        
+        answer.append(alpha)
     
-    return 1 if len(answer) == 0 else 0
+    return 0 if answer else 1 # answer가 비어 있지 않으면 0, 비어 있으면 1 return
