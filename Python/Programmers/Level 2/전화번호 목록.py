c1f84f8dbd2e17378e93f65f60b409b86700e459
김재훈 <kdk7121743@naver.com>
2021-11-05 01:16:26 +0900

    Update 전화번호 목록.py

--- Python/Programmers/Level 2/전화번호 목록.py
+++ Python/Programmers/Level 2/전화번호 목록.py
@@ -10,9 +10,17 @@
     
 #     return True
 
+# def solution(phone_book):
+#     phone_book.sort()
+#     for p1, p2 in zip(phone_book, phone_book[1:]): # zip을 이용하여 묶은 후 반복
+#         if p2.startswith(p1): # startswith를 이용하여 문자열이 접두어에 포함되는지 아닌지 확인
+#             return False
+#     return True
+
 def solution(phone_book):
     phone_book.sort()
-    for p1, p2 in zip(phone_book, phone_book[1:]): # zip을 이용하여 묶은 후 반복
-        if p2.startswith(p1): # startswith를 이용하여 문자열이 접두어에 포함되는지 아닌지 확인
+    for i in range(len(phone_book) - 1): # 붙어있는 요소들끼리 비교
+        if phone_book[i + 1].startswith(phone_book[i]):
             return False
+    
     return True
