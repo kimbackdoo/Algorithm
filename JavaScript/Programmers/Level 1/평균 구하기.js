b269eed07b1aadae35ae049905e108e5bf010650
김재훈 <kdk7121743@naver.com>
2022-01-12 00:14:30 +0900

    Update 평균 구하기.js

--- JavaScript/Programmers/Level 1/평균 구하기.js
+++ JavaScript/Programmers/Level 1/평균 구하기.js
@@ -1,3 +1,6 @@
-function solution(arr) {
-    return arr.reduce((sum, current) => sum + current, 0) / arr.length; // reduce 메소드를 이용하여 arr의 합을 구하고, arr의 길이만큼 나누어 평균을 구함
-}
+// function solution(arr) {
+//     return arr.reduce((sum, current) => sum + current, 0) / arr.length; // reduce 메소드를 이용하여 arr의 합을 구하고, arr의 길이만큼 나누어 평균을 구함
+// }
+
+// 함수 표현식 풀이
+const solution = (arr) => arr.reduce((total, num) => total + num, 0) / arr.length
