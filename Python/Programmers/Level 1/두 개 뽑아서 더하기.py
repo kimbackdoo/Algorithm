ec7e8bd54338d757d4f37a1fe974acdb1f46d8ee
김재훈 <kdk7121743@naver.com>
2022-01-11 23:41:21 +0900

    Update 두 개 뽑아서 더하기.py

--- Python/Programmers/Level 1/두 개 뽑아서 더하기.py
+++ Python/Programmers/Level 1/두 개 뽑아서 더하기.py
@@ -6,8 +6,28 @@
 #     answer = list(set(answer)) # 중복값 제거
 #     return sorted(answer) # 오름차순 정렬
 
-from itertools import combinations
+# from itertools import combinations
+
+# def solution(numbers):
+#     answer = list(map(sum, combinations(numbers, 2))) # map을 이용하여 2개 조합 덧셈 리스트 만들기
+#     return sorted(list(set(answer))) # 중복제거 및 오름차순 정렬
+
+# from itertools import combinations
+
+# def solution(numbers):
+#     return sorted(set(map(sum, combinations(numbers, 2)))) # map 결과는 set으로 만들고 오름차순 정렬
+
+# 백트레킹 풀이, dfs 알고리즘 이용
 
 def solution(numbers):
-    answer = list(map(sum, combinations(numbers, 2))) # map을 이용하여 2개 조합 덧셈 리스트 만들기
-    return sorted(list(set(answer))) # 중복제거 및 오름차순 정렬
+    def dfs(case, pick, idx): # dfs 정의
+        if len(case) == pick: # 뽑은 경우가 pick와 같다면
+            answer.add(sum(case)) # answer에 case의 합 추가
+            return # 재귀 탈출
+        
+        for i in range(idx, len(numbers)): # numbers의 모든 요소를 순회하면서 모든 경우 탐색
+            dfs(case + [numbers[i]], pick, i + 1) # dfs 탐색
+    
+    answer = set()
+    dfs([], 2, 0)
+    return sorted(answer)
