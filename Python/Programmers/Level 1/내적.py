24af18a0cefd2f2694c8c71a07e694e084d0a71f
김재훈 <kdk7121743@naver.com>
2022-01-12 23:53:01 +0900

    Update 내적.py

--- Python/Programmers/Level 1/내적.py
+++ Python/Programmers/Level 1/내적.py
@@ -1,2 +1,5 @@
-def solution(a, b): 
-    return sum([i * j for i, j in zip(a, b)]) # zip을 이용하여 곱셈
+# def solution(a, b): 
+#     return sum([i * j for i, j in zip(a, b)]) # zip을 이용하여 곱셈
+
+def solution(a, b):
+    return sum([x * y for x, y in zip(a, b)]) # map을 이용하여 곱셈
