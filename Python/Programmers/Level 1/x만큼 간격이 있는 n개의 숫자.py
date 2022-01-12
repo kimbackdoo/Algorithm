f908df32bce320e74e03767bd308716e9c1df27a
김재훈 <kdk7121743@naver.com>
2022-01-13 00:38:53 +0900

    Update x만큼 간격이 있는 n개의 숫자.py

--- Python/Programmers/Level 1/x만큼 간격이 있는 n개의 숫자.py
+++ Python/Programmers/Level 1/x만큼 간격이 있는 n개의 숫자.py
@@ -7,5 +7,11 @@
         
 #     return answer
 
+# def solution(x, n):
+#     return [(i+1) * x for i in range(n)] # 길이가 n 이어야 하므로 n번 반복하면서 x씩 증가하게 함
+
 def solution(x, n):
-    return [(i+1) * x for i in range(n)] # 길이가 n 이어야 하므로 n번 반복하면서 x씩 증가하게 함
+    # range 함수를 이용하여 숫자 범위를 만들고 리스트로 변환
+    # 범위의 마지막 값을 x * (n + 1)로 하여 양수, 음수일때 처리되도록함
+    # x가 0인 경우도 있으므로 0인 경우는 [0] * n을 return
+    return list(range(x, x * (n + 1), x)) if x else [0] * n
