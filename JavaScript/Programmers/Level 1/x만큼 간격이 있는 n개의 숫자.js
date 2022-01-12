35c98481d7059002d5b7a7e0101bbb063e32cf08
김재훈 <kdk7121743@naver.com>
2022-01-13 00:49:24 +0900

    Update x만큼 간격이 있는 n개의 숫자.js

--- JavaScript/Programmers/Level 1/x만큼 간격이 있는 n개의 숫자.js
+++ JavaScript/Programmers/Level 1/x만큼 간격이 있는 n개의 숫자.js
@@ -9,8 +9,14 @@
 //     return answer;
 // }
 
+// function solution(x, n) {
+//     // new Array를 이용하여 크기가 n인 배열을 만든 후, 모든 요소를 0으로 채움
+//     // 이후, map 메소드를 이용하여 각 요소를 (각 요소의 인덱스 + 1) * x로 mapping
+//     return new Array(n).fill(0).map((_, i) => (i + 1) * x);
+// }
+
 function solution(x, n) {
-    // new Array를 이용하여 크기가 n인 배열을 만든 후, 모든 요소를 0으로 채움
-    // 이후, map 메소드를 이용하여 각 요소를 (각 요소의 인덱스 + 1) * x로 mapping
-    return new Array(n).fill(0).map((_, i) => (i + 1) * x);
+    // 길이가 n인 배열을 만들고 x로 채움
+    // map 메소드를 이용하여 x씩 증가하게 만듦
+    return Array(n).fill(x).map((num, index) => num * (index + 1));;
 }
