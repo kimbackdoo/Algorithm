4542289f8333af37c7baeb0b800db06936831a0e
김재훈 <kdk7121743@naver.com>
2022-01-17 21:37:47 +0900

    Update 핸드폰 번호 가리기.py

--- Python/Programmers/Level 1/핸드폰 번호 가리기.py
+++ Python/Programmers/Level 1/핸드폰 번호 가리기.py
@@ -1,2 +1,5 @@
+# def solution(phone_number):
+#     return "*" * (len(phone_number) - 4) + phone_number[-4:] # "*"에 phone_number의 길이 -4 만큼 곱하고 뒤에 4자리 붙임
+
 def solution(phone_number):
-    return "*" * (len(phone_number) - 4) + phone_number[-4:] # "*"에 phone_number의 길이 -4 만큼 곱하고 뒤에 4자리 붙임
+    return "*" * len(phone_number[:-4]) + phone_number[-4:] # 뒤에 4자리를 제외하고 *를 붙임
