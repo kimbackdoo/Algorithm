52ffadce8661a1108b768c028a5a6851ccb89a9f
김재훈 <kdk7121743@naver.com>
2022-01-18 23:44:17 +0900

    Update 음양 더하기.py

--- Python/Programmers/Level 1/음양 더하기.py
+++ Python/Programmers/Level 1/음양 더하기.py
@@ -4,6 +4,14 @@
             
 #     return sum(absolutes)
 
+# def solution(absolutes, signs):
+#     # 리스트 컴프리헨션과 sum함수 이용하여 합을 구함
+#     return sum([absolutes[i] if signs[i] else -absolutes[i] for i in range(len(signs)) ])
+
+# # absolutes[i]가 길기 때문에 1, -1을 곱하는 방법을 사용
+# def solution(absolutes, signs):
+#     return sum([absolutes[i] * (1 if signs[i] else -1) for i in range(len(absolutes))])
+
+# zip 함수 사용
 def solution(absolutes, signs):
-    # 리스트 컴프리헨션과 sum함수 이용하여 합을 구함
-    return sum([absolutes[i] if signs[i] else -absolutes[i] for i in range(len(signs)) ])
+    return sum([a * (1 if s else -1) for a, s in zip(absolutes, signs)])
