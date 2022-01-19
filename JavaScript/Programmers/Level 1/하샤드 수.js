5b43eb34f0ea735d128059c901a9d30a2e032d7d
김재훈 <kdk7121743@naver.com>
2022-01-20 00:31:29 +0900

    Update 하샤드 수.js

--- JavaScript/Programmers/Level 1/하샤드 수.js
+++ JavaScript/Programmers/Level 1/하샤드 수.js
@@ -1,5 +1,10 @@
+// function solution(x) {
+//     // x를 문자열로 변환 후 배열로 변환, 이후 reduce 메소드를 이용하여 x의 각 자리의 합을 구함
+//     const num = [...(x + "")].reduce((sum, c) => sum + +c, 0);
+//     return !(x % num); // 나누어 떨어진다면 하샤드 수
+// }
+
 function solution(x) {
-    // x를 문자열로 변환 후 배열로 변환, 이후 reduce 메소드를 이용하여 x의 각 자리의 합을 구함
-    const num = [...(x + "")].reduce((sum, c) => sum + +c, 0);
-    return !(x % num); // 나누어 떨어진다면 하샤드 수
+    // split 메소드를 사용하여 x의 각 자리의 합을 구합
+    return !(x % (x + "").split("").reduce((sum, cur) => sum + +cur, 0));
 }
