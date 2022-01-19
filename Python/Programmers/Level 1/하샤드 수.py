017f42f3ae38bf19aaae202fedeba2eacea01c9c
김재훈 <kdk7121743@naver.com>
2022-01-20 00:18:44 +0900

    Update 하샤드 수.py

--- Python/Programmers/Level 1/하샤드 수.py
+++ Python/Programmers/Level 1/하샤드 수.py
@@ -1,3 +1,6 @@
-def solution(arr):
-    total = sum([int(num) for num in str(arr)]) # 각 자리수 합
-    return arr % total == 0
+# def solution(arr):
+#     total = sum([int(num) for num in str(arr)]) # 각 자리수 합
+#     return arr % total == 0
+
+def solution(x):
+    return bool(not x % sum(map(int, str(x)))) # sum, map 함수를 이용하여 각 자리수의 합을 구하고, bool 함수를 이용하여 boolean 값으로 변환
