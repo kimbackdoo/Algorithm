308999e49b4aedebbc208e351cfc70a9012cff00
김재훈 <kdk7121743@naver.com>
2022-01-20 00:04:30 +0900

    Update 3진법 뒤집기.py

--- Python/Programmers/Level 1/3진법 뒤집기.py
+++ Python/Programmers/Level 1/3진법 뒤집기.py
@@ -7,15 +7,26 @@
 #     answer = "".join(tmp) # tmp 리스트 문자열로 변환
 #     return int(answer, 3) # int 함수를 통해서 3진수 10진수로 변환
 
-def solution(n):
-    tmp = []
-    while n > 0: # 10진수 3진수 변환
-        tmp.append(n % 3)
-        n //= 3
+# def solution(n):
+#     tmp = []
+#     while n > 0: # 10진수 3진수 변환
+#         tmp.append(n % 3)
+#         n //= 3
     
-    tmp.reverse()
-    answer = 0
-    for i in range(len(tmp)): # 3진수 10진수 변환
-        answer += tmp[i] * (3**i)
+#     tmp.reverse()
+#     answer = 0
+#     for i in range(len(tmp)): # 3진수 10진수 변환
+#         answer += tmp[i] * (3**i)
         
-    return answer
+#     return answer
+
+def convert(n, base): # 진수 변환 함수
+    result = ""
+    while n:
+        result += str(n % base)
+        n //= base
+    
+    return result
+
+def solution(n):
+    return int(convert(n, 3), 3) # 3진수로 변환하고 int를 사용하여 다시 10진수로 
