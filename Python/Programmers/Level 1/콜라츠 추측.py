7c3a28b2c6afb41f26bc60ecc66b4f6f36e652c3
김재훈 <kdk7121743@naver.com>
2022-01-22 17:26:40 +0900

    Update 콜라츠 추측.py

--- Python/Programmers/Level 1/콜라츠 추측.py
+++ Python/Programmers/Level 1/콜라츠 추측.py
@@ -11,11 +11,20 @@
     
 #     return answer
 
+# def solution(n):
+#     answer = 0
+#     while n > 1:
+#         if answer == 500: return - 1 # 500번을 반복해도 1이 되지 않으면 -1 반환
+#         n = n // 2 if (n % 2) == 0 else (n * 3) + 1 # 1-1, 1-2 단계
+#         answer += 1 # count
+        
+#     return answer
+
 def solution(n):
     answer = 0
-    while n > 1:
-        if answer == 500: return - 1 # 500번을 반복해도 1이 되지 않으면 -1 반환
-        n = n // 2 if (n % 2) == 0 else (n * 3) + 1 # 1-1, 1-2 단계
-        answer += 1 # count
+    for _ in range(500): # for문을 이용하여 500번만 반복
+        if n == 1: return answer
+        n = n // 2 if (n % 2) == 0 else (n * 3) + 1
+        answer += 1
         
-    return answer
+    return -1
