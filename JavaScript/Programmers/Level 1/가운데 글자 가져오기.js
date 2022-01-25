345ff237f41760b0c554a514d0c8986bff0244a8
김재훈 <kdk7121743@naver.com>
2022-01-25 21:16:25 +0900

    Update 가운데 글자 가져오기.js

--- JavaScript/Programmers/Level 1/가운데 글자 가져오기.js
+++ JavaScript/Programmers/Level 1/가운데 글자 가져오기.js
@@ -2,6 +2,11 @@
 // s의 길이가 짝수일 경우 s.slice(글자의 길이 / 2, 글자의 길이 / 2 + 2)가 가운데 글자가 된다.
 // 즉, 홀수, 짝수일 경우 상관없이 s.slice(Math.floor((글자의 길이 - 1) / 2), Math.ceil((글자의 길이 - 1) / 2) + 1)이 가운데 글자가 된다.
 
+// function solution(s) {
+//     return s.slice(Math.floor((s.length - 1) / 2), Math.ceil((s.length - 1) / 2) + 1);
+// }
+
 function solution(s) {
-    return s.slice(Math.floor((s.length - 1) / 2), Math.ceil((s.length - 1) / 2) + 1);
+    // 올림, 내림 사용하지 않고 처리
+    return s.slice((s.length - 1) / 2, s.length / 2 + 1);
 }
