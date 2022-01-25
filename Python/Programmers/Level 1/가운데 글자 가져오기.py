e9c92a3a54fb6bcc49b662dc49be32d68aeb2e1f
김재훈 <kdk7121743@naver.com>
2022-01-25 21:11:40 +0900

    Update 가운데 글자 가져오기.py

--- Python/Programmers/Level 1/가운데 글자 가져오기.py
+++ Python/Programmers/Level 1/가운데 글자 가져오기.py
@@ -4,5 +4,11 @@
     
 #     return s[len(s)//2 : len(s)//2 + 1] # s 길이부터 s 길이 +1까지 split
 
+# def solution(s):
+#     return s[(len(s)-1)//2 : len(s)//2+1] # s 길이에서 -1 후 //2 연산부터 s길이 //2 연산 후 +1까지 split
+
+import math
+
 def solution(s):
-    return s[(len(s)-1)//2 : len(s)//2+1] # s 길이에서 -1 후 //2 연산부터 s길이 //2 연산 후 +1까지 split
+    # math 모듈을 사용하여 홀수일때는 가운데 글자 한개, 짝수일때는 가운데 글자 2개를 가져옴
+    return s[(len(s) - 1) // 2 : math.ceil((len(s) - 1) / 2) + 1]
