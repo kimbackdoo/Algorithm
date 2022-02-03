a61d3496ebd29a61fd4188027df18ecac0a3e632
김재훈 <kdk7121743@naver.com>
2022-02-03 21:59:43 +0900

    Update 수박수박수박수박수박수?.py

--- Python/Programmers/Level 1/수박수박수박수박수박수?.py
+++ Python/Programmers/Level 1/수박수박수박수박수박수?.py
@@ -15,5 +15,10 @@
 
 #     return answer
 
+# def solution(n):
+#     return ("수박"*n)[:n] # 수박 문자열에 n을 곱하고 n만큼 split
+
 def solution(n):
-    return ("수박"*n)[:n] # 수박 문자열에 n을 곱하고 n만큼 split
+    # 위 코드는 n이 증가하면 불필요한 메모리를 차지하는 단점이 있음
+    # (n + 1) // 2의 값만 곱하면 불필요한 메모리를 최소화하여 개선
+    return ("수박" * ((n + 1) // 2))[:n]
