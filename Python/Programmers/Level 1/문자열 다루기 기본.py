f08adaefaacddb5de8f0d0a6490c15c9f6d0bd73
김재훈 <kdk7121743@naver.com>
2022-02-07 23:23:09 +0900

    Update 문자열 다루기 기본.py

--- Python/Programmers/Level 1/문자열 다루기 기본.py
+++ Python/Programmers/Level 1/문자열 다루기 기본.py
@@ -1,12 +1,16 @@
-def isnum(s): # s가 숫자로만 이루어졌는지 확인
-    for word in s:
-        try:
-            int(word) # word가 숫자가 아니면
-        except:
-            return False # False 반환
+# def isnum(s): # s가 숫자로만 이루어졌는지 확인
+#     for word in s:
+#         try:
+#             int(word) # word가 숫자가 아니면
+#         except:
+#             return False # False 반환
         
-    return True
+#     return True
+
+# def solution(s):
+#     # s의 길이가 4 혹은 6이고, 숫자로만 구성되어있으면 True, 아니면 False 반환
+#     return (len(s) == 4 or len(s) == 6) and isnum(s)
 
 def solution(s):
-    # s의 길이가 4 혹은 6이고, 숫자로만 구성되어있으면 True, 아니면 False 반환
-    return (len(s) == 4 or len(s) == 6) and isnum(s)
+    # isdigit() 메소드를 사용하여 s가 숫자인지 판별
+    return (len(s) == 4 or len(s) == 6) and s.isdigit()
