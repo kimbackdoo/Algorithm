a7e9e8182faff97cdd70fd24471ca1c965717e02
김재훈 <kdk7121743@naver.com>
2022-02-07 23:35:35 +0900

    Update 문자열 내 p와 y의 개수.py

--- Python/Programmers/Level 1/문자열 내 p와 y의 개수.py
+++ Python/Programmers/Level 1/문자열 내 p와 y의 개수.py
@@ -8,9 +8,9 @@
         
 #     return "".join(result) # 리스트를 문자열 변환
 
-def lower(s): # 문자열 소문자로 변환 숏코딩
-    result = [chr(ord(word) + 32) if ord("A") <= ord(word) < ord("Z") else word for word in s]
-    return "".join(result)
+# def lower(s): # 문자열 소문자로 변환 숏코딩
+#     result = [chr(ord(word) + 32) if ord("A") <= ord(word) < ord("Z") else word for word in s]
+#     return "".join(result)
 
 # def count(s, w): # 해당하는 문자 개수 찾기
 #     cnt = 0
@@ -20,9 +20,21 @@ def lower(s): # 문자열 소문자로 변환 숏코딩
     
 #     return cnt
 
-def count(s, w): # 해당하는 문자 개수 찾기 숏코딩
-    return sum([1 for word in s if word == w])
+# def count(s, w): # 해당하는 문자 개수 찾기 숏코딩
+#     return sum([1 for word in s if word == w])
 
+# def solution(s):
+#     s = lower(s)
+#     return count(s, "p") == count(s, "y") # if, else를 사용안하고 == 연산자를 활용하여 True, False 반환
+
+# Counter 모듈을 사용하여 풀이
+# from collections import Counter
+
+# def solution(s):
+#     counter = Counter(s.lower())
+#     return counter["p"] == counter["y"]
+
+# count 메소드 사용하여 풀이
 def solution(s):
-    s = lower(s)
-    return count(s, "p") == count(s, "y") # if, else를 사용안하고 == 연산자를 활용하여 True, False 반환
+    s = s.lower()
+    return s.count("p") == s.count("y")
