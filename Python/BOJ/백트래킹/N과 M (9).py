9723ab65de9c490528e06fa6b25eabcf741b8bc2
김재훈 <kdk7121743@naver.com>
2022-02-15 23:47:00 +0900

    Update N과 M (9).py

--- Python/BOJ/백트래킹/N과 M (9).py
+++ Python/BOJ/백트래킹/N과 M (9).py
@@ -2,27 +2,49 @@
 # 수열이 중복되면 안되고, 사전 순으로 증가하는 순서로 출력해야함
 # dfs 알고리즘을 이용하여 순열을 구하면 됨, 이때 방문 처리와 해당 수열이 이미 존재하는지 확인해야함
 
-import sys
-input = sys.stdin.readline
+# import sys
+# input = sys.stdin.readline
+
+# n, m = map(int, input().split())
+# numbers = sorted(map(int, input().split())) # 사전 순으로 증가하는 순서로 출력해야하므로 입력받은 n개의 숫자들 오름차순 정렬
+
+# check = {} # 중복되는 순열이 있는지 확인하기 위한 딕셔너리
+# visited = [False] * n # 방문처리를 위한 리스트
+# def dfs(p): # dfs 구현
+#   if len(p) == m: # p의 길이가 m과 같다면 즉, m개를 뽑았다면
+#     result = " ".join(map(str, p)) # p를 공백을 기준으로 문자열로 변환
+#     if result not in check: # result가 check에 없다면 즉, 중복되지 않으면
+#       check[result] = 1 # result 방문 처리
+#       print(result) # result 출력
+
+#     return # 재귀 탈출
+  
+#   for i in range(n): # numbers의 모든 요소를 순회
+#     if not visited[i]: # i번째 요소를 방문하지 않았으면
+#       visited[i] = True # 방문 처리
+#       dfs(p + [numbers[i]]) # dfs 탐색
+#       visited[i] = False # 방문 취소
+
+# dfs([])
 
 n, m = map(int, input().split())
-numbers = sorted(map(int, input().split())) # 사전 순으로 증가하는 순서로 출력해야하므로 입력받은 n개의 숫자들 오름차순 정렬
+number = sorted(map(int, input().split()))
 
-check = {} # 중복되는 순열이 있는지 확인하기 위한 딕셔너리
-visited = [False] * n # 방문처리를 위한 리스트
-def dfs(p): # dfs 구현
-  if len(p) == m: # p의 길이가 m과 같다면 즉, m개를 뽑았다면
-    result = " ".join(map(str, p)) # p를 공백을 기준으로 문자열로 변환
-    if result not in check: # result가 check에 없다면 즉, 중복되지 않으면
-      check[result] = 1 # result 방문 처리
-      print(result) # result 출력
+def dfs(case):
+  if len(case) == m:
+    # 위 코드처럼 result 변수를 따로 두지 않고 case를 tuple로 변화하여 확인
+    if tuple(case) not in check:
+      print(*case)
+      check[tuple(case)] = True
 
-    return # 재귀 탈출
-  
-  for i in range(n): # numbers의 모든 요소를 순회
-    if not visited[i]: # i번째 요소를 방문하지 않았으면
-      visited[i] = True # 방문 처리
-      dfs(p + [numbers[i]]) # dfs 탐색
-      visited[i] = False # 방문 취소
+    return
 
+  for i in range(n):
+    if not visited[i]:
+      visited[i] = True
+      dfs(case + [number[i]])
+      visited[i] = False
+
+check = {}
+visited = [False] * n
 dfs([])
