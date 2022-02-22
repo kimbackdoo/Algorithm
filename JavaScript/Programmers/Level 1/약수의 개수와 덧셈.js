224b7c96e1d66ec212df34685aba4bf7ffacec39
김재훈 <kdk7121743@naver.com>
2022-02-23 00:40:21 +0900

    Update 약수의 개수와 덧셈.js

--- JavaScript/Programmers/Level 1/약수의 개수와 덧셈.js
+++ JavaScript/Programmers/Level 1/약수의 개수와 덧셈.js
@@ -13,13 +13,20 @@
 
 // answer 배열에 left부터 right까지 모든 숫자를 넣고 reduce 메소드를 이용하여 return
 
-function solution(left, right) {
-    let answer = [];
-    for (let num = left; num <= right; num++) {
-        answer.push(num); // left ~ right까지 모든 숫자 push
-    }
+// function solution(left, right) {
+//     let answer = [];
+//     for (let num = left; num <= right; num++) {
+//         answer.push(num); // left ~ right까지 모든 숫자 push
+//     }
     
-    return answer.reduce((accumulator, current)=> { // reduce 메소드를 이용하여 값을 구함
-        return accumulator + (Number.isInteger(current ** 0.5) ? -current : current); // 제곱근이 정수이면 뺄셈, 정수가 아니면 
-    }, 0);
+//     return answer.reduce((accumulator, current)=> { // reduce 메소드를 이용하여 값을 구함
+//         return accumulator + (Number.isInteger(current ** 0.5) ? -current : current); // 제곱근이 정수이면 뺄셈, 정수가 아니면 
+//     }, 0);
+// }
+
+// Array를 이용해서 배열을 만들고, fill 메소드를 이용해서 초기값 설정 후 reduce 메소드 사용
+function solution(left, right) {
+    return Array(right - left + 1)
+        .fill(0)
+        .reduce((answer, _, idx) => answer + (Number.isInteger((idx + left) ** 0.5) ? -(idx + left) : (idx + left)), 0);
 }
