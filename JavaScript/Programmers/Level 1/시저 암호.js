d344af13899f0ad8cd87ce1497d7d0ddac3d4c0f
김재훈 <kdk7121743@naver.com>
2022-02-23 00:21:38 +0900

    Update 시저 암호.js

--- JavaScript/Programmers/Level 1/시저 암호.js
+++ JavaScript/Programmers/Level 1/시저 암호.js
@@ -19,14 +19,29 @@
 
 // 아스키 코드 값으로 변환하여 계산하지 않고 미리 알파벳 문자열을 선언 후 계산
 
-function solution(s, n) {
-    const upper = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"; // 대문자 문자열
-    const lower = "abcdefghijklmnopqrstuvwxyz"; // 소문자 문자열
+// function solution(s, n) {
+//     const upper = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"; // 대문자 문자열
+//     const lower = "abcdefghijklmnopqrstuvwxyz"; // 소문자 문자열
     
-    return [...s].map(a => {
-        const alpha = upper.includes(a) ? upper : lower; // a가 대문자면 upper, 소문자면 lower 저장
-        // alpha에서 a의 인덱스가 -1이라는 뜻은 공백이라는 뜻
-        // 삼항 연산자를 이용하여 공백이면 공백 return, 공백이 아니면 n만큼 뒤로 민 알파벳 return
-        return alpha.indexOf(a) === -1 ? a : alpha[(alpha.indexOf(a) + n) % 26];
-    }).join(""); // 배열을 다시 문자열로 join
+//     return [...s].map(a => {
+//         const alpha = upper.includes(a) ? upper : lower; // a가 대문자면 upper, 소문자면 lower 저장
+//         // alpha에서 a의 인덱스가 -1이라는 뜻은 공백이라는 뜻
+//         // 삼항 연산자를 이용하여 공백이면 공백 return, 공백이 아니면 n만큼 뒤로 민 알파벳 return
+//         return alpha.indexOf(a) === -1 ? a : alpha[(alpha.indexOf(a) + n) % 26];
+//     }).join(""); // 배열을 다시 문자열로 join
+// }
+
+// reduce를 사용한 풀이
+function solution(s, n) {
+    return [...s].reduce((answer, alpha) => {
+        if (alpha === " ") {
+            return answer + alpha;
+        } else {
+            if (alpha === alpha.toUpperCase()) {
+                return answer + String.fromCharCode((alpha.charCodeAt() + n - 65) % 26 + 65);
+            } else if (alpha === alpha.toLowerCase()) {
+                return answer + String.fromCharCode((alpha.charCodeAt() + n - 97) % 26 + 97);
+            }
+        }
+    }, "");
 }
