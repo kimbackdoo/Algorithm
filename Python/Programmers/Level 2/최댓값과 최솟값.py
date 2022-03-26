cfcc9dbfce35d5073e577892d01497b155bd7f8c
김재훈 <kdk7121743@naver.com>
2022-03-26 19:36:18 +0900

    Update 최댓값과 최솟값.py

--- Python/Programmers/Level 2/최댓값과 최솟값.py
+++ Python/Programmers/Level 2/최댓값과 최솟값.py
@@ -1,5 +1,10 @@
 # s를 공백을 기준으로 나눈 후 int와 mapping 한 결과를 리스트로 변환
 
+# def solution(s):
+#     numbers = list(map(int, s.split(" "))) # s를 공백을 기준으로 split 후 int와 mapping 한 결과를 numbers에 저장
+#     return str(min(numbers)) + " " + str(max(numbers)) # numbers의 최솟값과 최댓값 반환
+
+# lambda식을 이용하여 정렬
 def solution(s):
-    numbers = list(map(int, s.split(" "))) # s를 공백을 기준으로 split 후 int와 mapping 한 결과를 numbers에 저장
-    return str(min(numbers)) + " " + str(max(numbers)) # numbers의 최솟값과 최댓값 반환
+    answer = sorted(s.split(" "), key=lambda x: int(x))
+    return " ".join([answer[0], answer[-1]])
