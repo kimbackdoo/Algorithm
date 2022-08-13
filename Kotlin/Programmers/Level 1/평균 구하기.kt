dd4902d6c236184d307940bb10cde1344181cd6a
김재훈 <kdk7121743@naver.com>
2022-08-13 13:09:02 +0900

    Create 평균 구하기.kt

---
+++ Kotlin/Programmers/Level 1/평균 구하기.kt
@@ -0,0 +1,13 @@
+// average 메소드를 통해 평균 구함
+class Solution {
+    fun solution(arr: IntArray): Double {
+        return arr.average()
+    }
+}
+
+// sum 메소드를 통해 arr의 합을 구하고, Double로 형변환 후 평균 구함
+class Solution {
+    fun solution(arr: IntArray): Double {
+        return arr.sum().toDouble() / arr.size
+    }
+}
