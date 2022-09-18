1b4da2a5d2d7c8b7ff802f37060c8d3b82fd3ed7
김재훈 <kdk7121743@naver.com>
2022-09-18 17:57:16 +0900

    Update H-Index.js

--- JavaScript/Programmers/Level 2/H-Index.js
+++ JavaScript/Programmers/Level 2/H-Index.js
@@ -1,4 +1,4 @@
-// H-Index는 주어진 논문들의 인용횟수를 내림차순 한 후 인덱스 값이 해당 요소 값 이상이면 해당 인덱스 값이 h-index가 된다.
+// H-Index는 주어진 논문들의 인용횟수를 내림차순 한 후 인덱스 값이 해당 요소 값 이상이면 해당 인덱스 값이 h-index가 됨
 
 function solution(citations) {
     citations.sort((a, b) => b - a); // 내림차순 정렬
