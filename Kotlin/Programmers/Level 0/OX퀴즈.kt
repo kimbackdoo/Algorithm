b14ca58bedb0667bb7f61b8d7d2a33a2f4321df0
김재훈 <kdk7121743@naver.com>
2022-12-13 00:00:35 +0900

    Update OX퀴즈.kt

--- Kotlin/Programmers/Level 0/OX퀴즈.kt
+++ Kotlin/Programmers/Level 0/OX퀴즈.kt
@@ -1,1 +1,31 @@
+// 문제에서는 단순히 X 연산자 Y = Z 형식이라고 했지만 조금 더 확장시켜서 풀기 위해 고민을 했던 문제
 
+class Solution {
+    fun solution(quiz: Array<String>): Array<String> {
+        val answer = mutableListOf<String>()
+        // quiz 모든 요소 순회
+        quiz.forEach { str ->
+            // str split 후 trim
+            val (expr, result) = str.split("=").map { it.trim() }
+            // expr 을 공백을 기준으로 split -> [숫자, 연산자, 숫자] 형식이 됨
+            val ops = expr.split(" ").toMutableList()
+            var tmp = 0
+            var index = 0
+            while (index < ops.size) {
+                // ops의 처음 요소부터 순회
+                val op = ops[index++]
+                tmp += when (op) {
+                    // +면 다음 숫자 더해줌
+                    "+"  -> ops[index++].toInt()
+                    // -면 다음 숫자 빼줌
+                    "-"  -> -ops[index++].toInt()
+                    // 숫자면 해당 숫자 더해줌
+                    else -> op.toInt()
+                }
+            }
+            val isAnswer = tmp == result.toInt()
+            answer += if (isAnswer) "O" else "X"
+        }
+        return answer.toTypedArray()
+    }
+}
